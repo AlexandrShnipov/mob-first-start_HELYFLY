59a54d9c658297042b5ea25c4e86778c7c1210bd
AlexandrShnipov <sshnipov@gmail.com>
2021-12-04 21:38:57 +0300

    added map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {HashRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import s from './App.module.scss';
 import Header from './components/blocks/header/Header';
 import Main from './components/blocks/main/Main';
@@ -6,21 +6,23 @@ import Description from './components/blocks/description/Description';
 import Gallery from './components/blocks/gallery/Gallery';
 import About from './components/blocks/about/About';
 import Recording from './components/blocks/recording/Recording';
+import Map from './components/blocks/map/Map';
 
 function App() {
   return (
 
     <HashRouter>
-        <div className={s.App}>
-      <Header/>
-      <Main/>
-      <Description/>
-      <Gallery/>
-      <About/>
-      <Recording/>
-    </div>
+      <div className={s.App}>
+        <Header />
+        <Main />
+        <Description />
+        <Gallery />
+        <About />
+        <Recording />
+        <Map/>
+      </div>
     </HashRouter>
-    );
+  );
 }
 
 export default App;
---
+++ src/components/blocks/map/Map.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import s from './Map.module.scss';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
+
+const Maps = () => (
+
+
+  <YMaps>
+    <div className={s.mapWrap}>
+      <Map state={{ center: [55.755500, 37.611787], zoom: 15 }}>
+        <Placemark
+          modules={['geoObject.addon.balloon']}
+          geometry={[55.755500, 37.611787]}
+          options={{
+            iconColor: 'rgb(255,0,0)'
+          }}
+          properties={{
+            // balloonContentHeader: "Балун метки"
+            balloonContentBody: 'Встречаемся тут!'
+            // balloonContentFooter: "Подвал"
+          }} />
+      </Map>
+    </div>
+  </YMaps>
+);
+
+export default Maps;
